75a62f42ff58616aa81a305f986b8b87a836057d
解豪洲 <2983537233@qq.com>
2026-04-27 16:24:27 +0800

    feat: 精简后台路由菜单

---
+++ mgr/webui/src/pages/Users/List/index.tsx
@@ -0,0 +1,11 @@
+import { SettingOutlined } from "@ant-design/icons";
+
+export function UserListPage() {
+  return (
+    <section className="empty-page">
+      <SettingOutlined />
+      <h2>用户列表</h2>
+      <p>开发中</p>
+    </section>
+  );
+}
